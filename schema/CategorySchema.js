3b54a991b2ab44de59918ddb56aad39d6f6b8f3e
Thomas Hessburg <44884308+TomHessburg@users.noreply.github.com>
2019-10-05 01:06:41 -0400

    added category and material schemas

---
+++ schema/CategorySchema.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const category = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  unit: {
+    type: String,
+    required: true
+  }
+});
+
+const Category = mongoose.model("category", category);
+module.exports = Category;
---
+++ schema/MaterialSchema.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const material = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String
+    },
+    photoUrl: {
+      type: String
+    },
+    grade: {
+      type: String
+    },
+    category_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "category",
+      required: true
+    }
+  },
+  { timestamps: true }
+);
+
+const Material = mongoose.model("material", material);
+module.exports = Material;
